d5a71c18c3b04a80972eace463579fa0591655a2
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com>
2025-06-14 22:05:59 -0300

    refactor: update chat blueprint import and modify schema for primary key type

--- src/flaskr/chat/__init__.py
+++ src/flaskr/chat/__init__.py
@@ -1,13 +1,1 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint(
-    'chat',
-    __name__,
-    template_folder='templates', 
-    static_folder='../static', 
-    url_prefix='/chat'
-)
-
-@bp.route('/')    
-def index():
-    return render_template('chat.html')
+from .views import bp
